b5c39ae54af952564d2ba5b199a2816c30f55d18
PDKetchum <pxiong265@gmail.com>
2022-09-25 18:17:48 -0500

    added get and post for apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,3 +1,34 @@
 const apiRoutes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
+
+apiRoutes.get("/api/notes", (req, res) =>
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+);
+
+apiRoutes.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    console.log("Error in posting note");
+    res.json("Error in posting note");
+  }
+});
 
 module.exports = apiRoutes;
